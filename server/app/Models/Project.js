1d9b2f23443663e29696bbd56f69f6cf7bbbb384
Abhishek <abhishek.gorisaria@alienbrains.in>
2020-06-11 16:58:04 +0530

    task model added

--- server/app/Models/Project.js
+++ server/app/Models/Project.js
@@ -7,6 +7,9 @@ class Project extends Model {
     user(){
         return this.belongsTo('App/Models/User');
     }
+    tasks () {
+        return this.hasMany('App/Models/Task')
+    }
 }
 
 module.exports = Project
