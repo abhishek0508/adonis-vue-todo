1e06af054e3923fe3cccabfb5a1f0629eaadd8e9
Abhishek <abhishek.gorisaria@alienbrains.in>
2020-06-11 16:09:04 +0530

    Resource Destroy done

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Project = use('App/Models/Project')
+const AuthorizationService = use('App/Services/AuthorizationService')
+
 class ProjectController {
     async index({ auth }){
         const user = await auth.getUser();
@@ -16,6 +18,15 @@ class ProjectController {
         });
 
         await user.projects().save(project);
+        return project;
+    }
+
+    async destroy({ auth, request, params }){
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        AuthorizationService.verifyPermission(project,user);
+        await project.delete();
         return project;
     }
 }
